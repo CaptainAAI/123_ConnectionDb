fee547b2e39c0b5765cd3750f280d586e93cfc9c
Ananda Agung Ismail <158981471+CaptainAAI@users.noreply.github.com>
2025-10-15 11:53:23 +0700

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors');
+const cors = require('cors'); 
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
